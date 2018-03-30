ad407f290a177109e0f87fcf7fe622f043c9ffc8
julyL <849424162@qq.com>
2018-03-30 20:22:17 +0800

    koa-static

---
+++ koa2/koa2源码/koa-static/koa-send.js
@@ -0,0 +1,184 @@
+/**
+ * Module dependencies.
+ */
+
+const debug = require('debug')('koa-send')
+const resolvePath = require('resolve-path')
+const createError = require('http-errors')
+const assert = require('assert')
+const fs = require('mz/fs')
+
+const {
+    normalize,
+    basename,
+    extname,
+    resolve,
+    parse,
+    sep
+} = require('path')
+
+/**
+ * Expose `send()`.
+ */
+
+module.exports = send
+
+/**
+ * Send file at `path` with the
+ * given `options` to the koa `ctx`.
+ *
+ * @param {Context} ctx
+ * @param {String} path
+ * @param {Object} [opts]
+ * @return {Function}
+ * @api public
+ */
+
+async function send(ctx, path, opts = {}) {
+    assert(ctx, 'koa context required')
+    assert(path, 'pathname required')
+
+    // options
+    debug('send "%s" %j', path, opts)
+    const root = opts.root ? normalize(resolve(opts.root)) : ''
+    const trailingSlash = path[path.length - 1] === '/'
+    // 从path中去掉root部分的字符  eg: root/xxx/file.js => xxx/file.js
+    path = path.substr(parse(path).root.length)
+    const index = opts.index
+    const maxage = opts.maxage || opts.maxAge || 0
+    const immutable = opts.immutable || false
+    const hidden = opts.hidden || false
+    const format = opts.format !== false
+    const extensions = Array.isArray(opts.extensions) ? opts.extensions : false
+    const brotli = opts.brotli !== false
+    const gzip = opts.gzip !== false
+    const setHeaders = opts.setHeaders
+
+    if (setHeaders && typeof setHeaders !== 'function') {
+        throw new TypeError('option setHeaders must be function')
+    }
+
+    // normalize path
+    path = decode(path)
+
+    if (path === -1) return ctx.throw(400, 'failed to decode')
+
+    // index file support
+    if (index && trailingSlash) path += index
+
+    path = resolvePath(root, path)
+
+    // hidden file support, ignore
+    if (!hidden && isHidden(root, path)) return
+
+    let encodingExt = ''
+    // serve brotli file when possible otherwise gzipped file when possible
+    // 如果client端接受压缩 && 服务端设置了压缩  && 能在本地找到有相应压缩后缀的文件
+    if (ctx.acceptsEncodings('br', 'identity') === 'br' && brotli && (await fs.exists(path + '.br'))) {
+        path = path + '.br'
+        ctx.set('Content-Encoding', 'br')
+        ctx.res.removeHeader('Content-Length')
+        encodingExt = '.br'
+    } else if (ctx.acceptsEncodings('gzip', 'identity') === 'gzip' && gzip && (await fs.exists(path + '.gz'))) {
+        path = path + '.gz'
+        ctx.set('Content-Encoding', 'gzip')
+        ctx.res.removeHeader('Content-Length')
+        encodingExt = '.gz'
+    }
+
+    // 设置extensions && path中没有后缀,则会添加后缀之后再查找文件
+    // eg: extensions中有png, 当请求 /banner时, 会自动查找 /banner.png
+    if (extensions && !/\.[^/]*$/.exec(path)) {
+        const list = [].concat(extensions)
+        for (let i = 0; i < list.length; i++) {
+            let ext = list[i]
+            if (typeof ext !== 'string') {
+                throw new TypeError('option extensions must be array of strings or false')
+            }
+            if (!/^\./.exec(ext)) ext = '.' + ext   // png => .png
+            if (await fs.exists(path + ext)) {      // 添加后缀之后再进行查找
+                path = path + ext
+                break
+            }
+        }
+    }
+
+    // stat
+    let stats
+    try {
+        stats = await fs.stat(path)   // 根据path判断是否存在相应的本地资源
+
+        // Format the path to serve static file servers
+        // and not require a trailing slash for directories,
+        // so that you can do both `/directory` and `/directory/`
+        if (stats.isDirectory()) {  // 判断是否为目录
+            if (format && index) {
+                path += '/' + index     // 返回index对应的默认资源 (koa-static默认为index.html)
+                stats = await fs.stat(path)
+            } else {
+                return
+            }
+        }
+    } catch (err) {
+        // ENOENT: 表明指定的路径不存在，即给定的路径找不到文件或目录
+        // ENOTDIR: 给定的路径虽然存在，但不是一个目录
+        // ENAMETOOLONG: 文件名过长
+        const notfound = ['ENOENT', 'ENAMETOOLONG', 'ENOTDIR']
+        if (notfound.includes(err.code)) {  // 上面3个err属于预料范畴内的错误, 属于程序正常运行的情况 
+            throw createError(404, err)
+        }
+        // 其他情况则直接报500
+        err.status = 500
+        throw err
+    }
+
+    if (setHeaders) setHeaders(ctx.res, path, stats)
+
+    // 用文件大小设置Content-Length
+    ctx.set('Content-Length', stats.size)
+    // 没有设置过Last-Modified,则用资源最近的修改时间作为Last-Modified
+    if (!ctx.response.get('Last-Modified')) ctx.set('Last-Modified', stats.mtime.toUTCString())
+    if (!ctx.response.get('Cache-Control')) {
+        const directives = ['max-age=' + (maxage / 1000 | 0)]
+        if (immutable) {
+            directives.push('immutable')
+        }
+        ctx.set('Cache-Control', directives.join(','))
+    }
+    ctx.type = type(path, encodingExt)     // 设置Content-Type
+    ctx.body = fs.createReadStream(path)   // 资源会转换为可读流, 最终通过body.pipe(res)返回给client端
+
+    return path
+}
+
+/**
+ * Check if it's hidden.
+ */
+
+function isHidden(root, path) {
+    path = path.substr(root.length).split(sep)
+    for (let i = 0; i < path.length; i++) {
+        if (path[i][0] === '.') return true
+    }
+    return false
+}
+
+/**
+ * File type.
+ */
+
+function type(file, ext) {
+    return ext !== '' ? extname(basename(file, ext)) : extname(file)
+}
+
+/**
+ * Decode `path`.
+ */
+
+function decode(path) {
+    try {
+        return decodeURIComponent(path)
+    } catch (err) {
+        return -1
+    }
+}
---
+++ koa2/koa2源码/koa-static/koa-static.js
@@ -0,0 +1,74 @@
+
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+const debug = require('debug')('koa-static')
+const { resolve } = require('path')
+const assert = require('assert')
+const send = require('koa-send')
+
+/**
+ * Expose `serve()`.
+ */
+
+module.exports = serve
+
+/**
+ * Serve static files from `root`.
+ *
+ * @param {String} root
+ * @param {Object} [opts]
+ * @return {Function}
+ * @api public
+ */
+
+function serve(root, opts) {
+    opts = Object.assign({}, opts)
+
+    assert(root, 'root directory is required to serve files')
+
+    // options
+    debug('static "%s" %j', root, opts)
+    opts.root = resolve(root)  // 设置静态资源的根目录
+    if (opts.index !== false) opts.index = opts.index || 'index.html'  //没有设置index,默认返回index.html
+
+    if (!opts.defer) {  // 默认opts.defer就为假值,等到send方法处理之后再执行next
+        return async function serve(ctx, next) {
+            let done = false
+            // HEAD: 只返回资源的头部信息  GET: 返回资源的头部和主体内容   只有这2个请求可以视为是请求静态资源
+            if (ctx.method === 'HEAD' || ctx.method === 'GET') {
+                try {
+                    done = await send(ctx, ctx.path, opts)
+                } catch (err) {
+                    if (err.status !== 404) {  // 这里status只可能为404和500, 404是没有找到资源时主动返回的属于正常情况,500属于程序运行发生的异常情况需要处理
+                        throw err
+                    }
+                }
+            }
+
+            if (!done) {
+                await next()
+            }
+        }
+    }
+    // opts.defer设置为true时, 会等到后续中间件执行完之后在进行处理
+    return async function serve(ctx, next) {
+        await next()
+
+        if (ctx.method !== 'HEAD' && ctx.method !== 'GET') return
+        // response is already handled
+        // body不会null或者stats不为404, 说明其他中间件已经能够正确这次请求,不需要send方法再进行处理,否则会覆盖其他中间件的处理结果
+        if (ctx.body != null || ctx.status !== 404) return // eslint-disable-line
+
+        try {
+            await send(ctx, ctx.path, opts)
+        } catch (err) {
+            if (err.status !== 404) {
+                throw err
+            }
+        }
+    }
+}
