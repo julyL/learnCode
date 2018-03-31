6891ef79dcf48fae2e95edd8ecfead3e3c652e99
julyL <849424162@qq.com>
2018-03-31 20:51:12 +0800

    read koa-router

--- koa2/demo/index.js
+++ koa2/demo/index.js
@@ -1,20 +1,22 @@
 const Koa = require("koa");
 const static = require("koa-static");
 const path = require("path");
+const Router = require('koa-router');
 const logger = require('./logger.js');
 const app = new Koa();
-const staticPath = path.join(__dirname, "./static");
-// app.use(static(staticPath));
+const staticPath = path.join(__dirname, ".");
+app.use(static(staticPath));
 
+var router = new Router();
 
-app.use(async(ctx, next) => {
-  console.log('before');
-  ctx.body = "hello koa2";
+router.get("/user/:id", (ctx, next) => {
+  console.log(ctx.params);
+  ctx.body = "word";
   next();
-  console.log('after');
 });
 
-// app.use(logger);
+
+app.use(router.routes());
 
 app.listen(3000);
 console.log("[demo] start-quick is starting at port 3000");
